109ff9958f6aa67f7d491faafe5ccba6a2f35d34
bad974415boy-sys <bad974415boy@gmail.com>
2026-06-04 10:54:32 +0530

    task commit

--- backend/server.js
+++ backend/server.js
@@ -122,6 +122,5 @@ app.delete("/tasks/:id", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("🚀 Server running on port 3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
